daf4e92e2bcc0fd76e56364e5a5be46b36d3d35a
EC2 Default User <ec2-user@ip-172-31-7-6.ap-southeast-1.compute.internal>
2021-05-26 06:11:20 +0000

    Update repository

--- nodejs/test/text.js
+++ nodejs/test/text.js
@@ -3,12 +3,12 @@ let { registerFont, createCanvas } = require('canvas');
 describe('Canvas', function() {
   describe('#registerFont', function() {
     it('should register defined Font by name', function() {
-      registerFont('/usr/share/fonts/google-noto/NotoSansGothic-Regular.ttf', { family: 'Noto Sans Gothic Regular' });
+      registerFont('/usr/share/fonts/dejavu/DejaVuSans.ttf', { family: 'DejaVu Sans' });
     });
   }),
   describe('#fillText', function() {
     it('should write defined Text in canvas', function() {
-      registerFont('/usr/share/fonts/google-noto/NotoSansGothic-Regular.ttf', { family: 'Noto Sans Gothic Regular' });
+      registerFont('/usr/share/fonts/dejavu/DejaVuSans.ttf', { family: 'DejaVu Sans' });
       let canvas = createCanvas(300, 300);
       let ctx1 = canvas.getContext('2d');
       ctx1.font = '30px';
@@ -17,8 +17,8 @@ describe('Canvas', function() {
       ctx2.font = '30px "DejaVu Sans"';
       ctx2.fillText('DejaVu Sans', 5, 45);
       let ctx3 = canvas.getContext('2d');
-      ctx3.font = '30px "Noto Sans Gothic Regular"';
-      ctx3.fillText('Noto Sans Gothic Regular', 5, 75);
+      ctx3.font = '30px "DejaVu Sans Bold"';
+      ctx3.fillText('DejaVu Sans Bold', 5, 75);
       console.log(canvas.toDataURL());
     })
   });
