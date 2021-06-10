2e5d171e90437ee35ca467af97a8615aed7e4caf
EC2 Default User <ec2-user@ip-172-31-7-6.ap-southeast-1.compute.internal>
2021-06-10 09:33:59 +0000

    Update dependencies and add SVG support

--- build-layer.sh
+++ build-layer.sh
@@ -1,2 +1,9 @@
-rm ../canvas-nodejs_v2.8.0.zip 
+cd nodejs
+rm -rf node_modules package*.json
+npm init -y
+npm install canvas --build-from-source
+npm install mocha --save-dev
+cp package-lock.json ..
+cd ..
+rm ../canvas-nodejs_v2.8.0.zip
 zip -r ../canvas-nodejs_v2.8.0.zip . -x "LICENSE" "README.md" ".git*" "nodejs/test/*" "*.yml" "build-layer.sh"
--- nodejs/node_modules/canvas/build/Release/.deps/Release/canvas.node.d
+++ nodejs/node_modules/canvas/build/Release/.deps/Release/canvas.node.d
@@ -1,1 +1,1 @@
-cmd_Release/canvas.node := rm -rf "Release/canvas.node" && cp -af "Release/obj.target/canvas.node" "Release/canvas.node"
+cmd_Release/canvas.node := ln -f "Release/obj.target/canvas.node" "Release/canvas.node" 2>/dev/null || (rm -rf "Release/canvas.node" && cp -af "Release/obj.target/canvas.node" "Release/canvas.node")
