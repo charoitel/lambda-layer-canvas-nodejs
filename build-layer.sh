2f6202a630cbd450cc968525ea3594d0a02e1f77
EC2 Default User <ec2-user@ip-172-31-7-6.ap-southeast-1.compute.internal>
2021-06-10 15:35:13 +0000

    Update build layer script

--- build-layer.sh
+++ build-layer.sh
@@ -3,7 +3,11 @@ rm -rf node_modules package*.json
 npm init -y
 npm install canvas --build-from-source
 npm install mocha --save-dev
+CANVAS_VERSION=$(jq -r '.dependencies.canvas[1:]' package.json)
+jq --arg CANVAS_VERSION "$CANVAS_VERSION" '.name = "canvas-nodejs" | .version = $CANVAS_VERSION | .license = "MIT" | .author = "Charoite Lee" | .scripts.test = "mocha"' package.json > package-tmp.json
+mv -f package-tmp.json package.json
+npm test
 cp package-lock.json ..
 cd ..
-rm ../canvas-nodejs_v2.8.0.zip
-zip -r ../canvas-nodejs_v2.8.0.zip . -x "LICENSE" "README.md" ".git*" "nodejs/test/*" "*.yml" "build-layer.sh"
+rm ../canvas-nodejs_v$CANVAS_VERSION.zip
+zip -q -r ../canvas-nodejs_v$CANVAS_VERSION.zip . -x "LICENSE" "README.md" ".git*" "nodejs/test/*" "*.yml" "build-layer.sh"
