4e8bc1519b4db4d6995f6b47f663b4f49f3b7be4
charoitel <charoitel@yandex.com>
2023-01-03 03:31:19 +0000

    Fix font file path

--- build-layer.sh
+++ build-layer.sh
@@ -6,7 +6,7 @@
 # yum install gcc-c++ cairo-devel pango-devel libjpeg-turbo-devel giflib-devel librsvg2-devel pango-devel bzip2-devel jq python3 -y
 #
 # Setting Up Node.js, refer https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-up-node-on-ec2-instance.html for details
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 # . ~/.nvm/nvm.sh
 # nvm install --lts
 #
